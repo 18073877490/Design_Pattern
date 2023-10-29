fae85fbe6e83929f20b6206f3ce5845083c98bbd
xipian <2465750927@qq.com>
2023-10-29 21:14:13 +0800

    Commit and Push

---
+++ src/demo/Client.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Client {
+    public static void main(String[] args) {
+//        1、创建搜狗输入法对象
+        SougoInput input = new SougoInput();
+//        2、创建需要的皮肤对象
+        DefaultSpecificSkin defaultSpecificSkin = new DefaultSpecificSkin();
+        MySkin mySkin = new MySkin();
+//        3、选择需要的皮肤到输入法中
+        input.setSkin(mySkin);
+//        4、通过输入法对象展示皮肤
+        input.display();
+    }
+}
