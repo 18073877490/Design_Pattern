a05bd358dbc4981878c3f255a7d21208e3354eb0
xipian <2465750927@qq.com>
2023-10-29 23:00:19 +0800

    Commit and Push

---
+++ src/demo/DefaultSpecificSkin.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class DefaultSpecificSkin extends AbstractSkin {
+    @Override
+    public void display() {
+        System.out.println("这是默认皮肤");
+    }
+}
---
+++ src/demo/MySkin.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class MySkin extends AbstractSkin {
+    @Override
+    public void display() {
+        System.out.println("这是我的皮肤");
+    }
+}
